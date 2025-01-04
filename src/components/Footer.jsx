a25d7276eec5c30c6029d62da06325bf2560a9a6
sumonDev9 <sumonmit678@gmail.com>
2025-01-05 01:33:53 +0530

    skills components card design updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer className="footer footer-center bg-base-200 text-base-content rounded p-10">
-        <nav className="grid grid-flow-col gap-4">
+        <nav className="grid grid-flow-col text-primary text-base gap-4">
           <a className="link link-hover">Home</a>
           <a className="link link-hover">About</a>
           <a className="link link-hover">Skills</a>
@@ -47,7 +47,7 @@ const Footer = () => {
           </div>
         </nav>
         <aside>
-          <p>Copyright © {new Date().getFullYear()} - All right reserved by Sumon Mitra</p>
+          <p className='text-primary text-base'>Copyright © {new Date().getFullYear()} - All right reserved by Sumon Mitra</p>
         </aside>
       </footer>
     );
